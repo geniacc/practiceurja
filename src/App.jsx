6d8e88022e3199b292946eaf0e9c021a71dfe528
Arman SIngh <armanprabhat123912@gmail.com>
2025-07-06 23:48:51 +0530

    fix: use HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // src/App.jsx
 import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
